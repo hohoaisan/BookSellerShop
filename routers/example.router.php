1ea1a5a283584475ae619bd28be172e23879255e
Hoai San <hohoaisan@gmail.com>
2020-07-03 19:20:39 +0700

    implement Bramus Router

---
+++ routers/example.router.php
@@ -0,0 +1,14 @@
+<?php
+include_once('../vendor/autoload.php');
+// Lấy url của router và xử lí để tránh lỗi, url sẽ được đưa vào các thẻ a, form, ... liên quan đến router hiện tại
+$_SERVER['REQUEST_URI'] = "/" . trim($_SERVER['REQUEST_URI'], "/");
+// Đưa các hàm trong controller vào router
+include_once('../controllers/example.controller.php');
+
+// $router chính là $router từ index.php truyền sang bằng use($router),
+//nhằm tiếp nhận những đưỡng dẫn phía sau /admin/... thông qua hàm mount
+$router->get('/', $getpostdatabaseexample);
+$router->get('/subadmin', $subadmin);
+$router->post('/subadmin', $postSubadmin);
+// http://localhost/example/haha/subpath/bb
+$router->get('/{param1}/subpath/{param2}', $routeparamexample);
