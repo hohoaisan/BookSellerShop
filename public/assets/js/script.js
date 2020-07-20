148ff4ca972fe391aaf12c4acd1d7b92d932e946
Accel <cyberpunk2076@gmail.com>
2020-07-20 11:30:02 +0700

    remove scrolable

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -146,12 +146,3 @@ $(document).ready(function () {
     trigger: 'focus',
   });
 });
-
-//keep Scrooling Position
-window.addEventListener('scroll', () => {
-  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-  const scrolled = window.scrollY;
-  if (Math.ceil(scrolled) === scrollable) {
-    alert('end of page');
-  }
-});
