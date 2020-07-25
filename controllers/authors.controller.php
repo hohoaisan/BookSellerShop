5f04c42b302f0704759fe826732a381aae2fedf1
Accel <cyberpunk2076@gmail.com>
2020-07-25 11:18:46 +0700

    add author detail

--- controllers/authors.controller.php
+++ controllers/authors.controller.php
@@ -34,9 +34,41 @@
     
         echo PugFacade::displayFile('../views/home/author.jade', [
         'listAuthors' => $result,
+        'num_records' => $num_records,
         'pagination' => $pagination,
         'pagination_current_page' => $currentPage
         ]);
         exit();
-    }
+    };
+
+    $authorDetail = function($authorid) use ($removeParam){
+        try {
+            $page = intval(isset($_GET['page']) ? $_GET['page'] : 1);
+        } catch (Exception $e) {
+            $page = 1;
+        }
+        $itemperpage = 12;
+    
+        $currentAuthor = AuthorModel::getSingleAuthor($authorid);
+
+        $fetch = AuthorModel::getAuthorBooks($authorid, $page, $itemperpage);
+        $result = $fetch['result']; //Lấy kết quả trong 1 trang pagination
+    
+        $num_records = $fetch['rowcount']; //Lấy số kết quả trong toàn bộ bảng
+        $num_page = ceil($num_records / $itemperpage); //Số trang
+    
+        if (!$fetch) {
+            $result = [];
+        }
+    
+        echo PugFacade::displayFile('../views/home/authorDetail.jade', [
+        'listBooks' => $result,
+        'author' => $currentAuthor,
+        'pagination_url' => $removeParam('page'), //Lấy url cũ và render mới
+        'pagination_pages' => $num_page,
+        'pagination_current_page' => $page
+        ]);
+        exit();
+        // echo PugFacade::displayFile('../views/home/authorDetail.jade');
+    };
 ?>
